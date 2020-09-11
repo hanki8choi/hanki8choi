4e991650075550212392edc63402a9d49f7094bc
hanki8.choi <coffa80@naver.com>
2020-09-12 01:11:30 +0900

    완성 버전1

--- TickerMng.py
+++ TickerMng.py
@@ -1,115 +1,113 @@
 import pandas as pd
 import pandas_datareader as pdr
-import operator
 import csv
-import FinanceDataReader as fdr
+
 
 class TickerMng:
     def __init__(self):
-        self.mode = 0  #
+        # 그룹별 종목 이름 리스트
         self.offenseNames = []
         self.defenseNames = []
         self.canaryNames = []
-        self.offense_dfs = [pd.DataFrame()]
-        self.defense_dfs = [pd.DataFrame()]
-        self.canary_dfs = [pd.DataFrame()]
-        
-        self.namesGroup = {'offense':self.offenseNames, 'defense':self.defenseNames, 'canary':self.canaryNames}
-        self.dfsGroup = {'offense':self.offense_dfs, 'defense':self.defense_dfs, 'canary':self.canary_dfs}
 
-    def make_ticker2dataframe(self, type, ticker_name):
-        mon = pd.read_csv('data/M_%s.csv' % (ticker_name))
-        #for idx in range( 12, len(mon)): 
-        for idx in range( 0, len(mon) ): 
-            score = mon.pct_change(periods=1)[idx] * 12 + mon.pct_change(periods=3)[idx] * 4 + mon.pct_change(periods=6)[idx] * 2 + mon.pct_change(periods=12)[idx]
-            mon['score'] = score
-            
-        for idx in range( 0, len(mon)-1 ): 
-            profit = mon['Close'][idx+1] - mon['Close'][idx]
-            mon['profit'] = profit
-            
-        dfs = self.dfsGroup[type]
-        dfs[ticker_name] = mon
-        
-        
+        # 그룹별 종목 DataFrame 배열
+        self.offense_dfs = {}
+        self.defense_dfs = {}
+        self.canary_dfs = {}
+
+        # 집합 그룹
+        self.groupsNames = {'offense': self.offenseNames, 'defense': self.defenseNames, 'canary': self.canaryNames}
+        self.groups_dfs = {'offense': self.offense_dfs, 'defense': self.defense_dfs, 'canary': self.canary_dfs}
+
+    def make_ticker2dataframe(self, group_name, ticker_name):
+        mon = pd.read_csv('data/M_%s.csv' % ticker_name , sep='\t' )
+        mon = mon.set_index('Date', drop=True)
+
+        score_df = mon.pct_change(periods=1) * 12 + mon.pct_change(periods=3) * 4 + \
+                   mon.pct_change(periods=6) * 2 + mon.pct_change(periods=12)
+
+        score_df.columns = ['Score']
+
+        profit_df = mon.pct_change(periods=1)
+        profit_df.columns = ['Profit']
+
+        mon = pd.concat([mon, score_df['Score']] , axis=1 )
+        mon = pd.concat([mon, profit_df['Profit']], axis=1)
+
+        self.groups_dfs[group_name][ticker_name] = mon
+
+
     def load_type_list(self):
-        for key, val in self.namesGrouop.items():
+        for key, val in self.groupsNames.items():
             val.clear()
-            fd_list = open('data/Z_%s_list.csv' % (key), 'r', encoding='utf-8')
+            fd_list = open('data/Z_%s_list.csv' % key, 'r', encoding='utf-8')
             rdlist = csv.reader(fd_list)
             for line in rdlist:
                 ticker_name = line[0]
-                val.append(ticker_name)                
+                val.append(ticker_name)
             fd_list.close()
 
     def download_stooq(self):
-        for key, val in self.namesGroup.items():
+        for key, val in self.groupsNames.items():
             for ticker_name in val:
                 df = pdr.get_data_stooq(ticker_name)
-                df.to_csv( 'data/O_%s.csv'%(ticker_name) )
-                mon_df = df['Close'].resample('M').first()
-                mon_df.to_csv('data/M_%s.csv'%(ticker_name) )
-
-
-    def download_fdr(self):
-        for key, val in self.namesGroup.items():
-            for ticker_name in val:
-                #2019년에서 현재까지
-                fdr = fdr.DataReader(ticker_name, '2019' )
-                df.to_csv( 'data/O_%s.csv'%(ticker_name) )
-                mon_df = df['Close'].resample('M').first()
-                mon_df.to_csv('data/M_%s.csv'%(ticker_name) )
-
+                df.to_csv('data/O_%s.csv' % ticker_name, sep='\t' )
+                mon_df = df['Close'].resample('M').first().to_frame('Close')
+                mon_df.to_csv('data/M_%s.csv' % ticker_name, sep='\t' )
 
     def load_dataframe(self):
-        for key, val in self.namesGroup.items():
+        for key, val in self.groupsNames.items():
             for ticker_name in val:
-                make_ticker2dataframe(key, ticker_name)
+                self.make_ticker2dataframe(key, ticker_name)
 
-    def is_risky_canary(self, date_idx )
-        for val in self.canary_dfs:
-            if val['score'].iloc[date_idx] <= 0 :
+    def is_risky_canary(self, date_idx):
+        for key, val in self.canary_dfs.items():
+            if val['Score'].iloc[date_idx] <= 0:
                 return True
         return False
-        
-    def select_defense(self, date_idx )
-        names = sort_dfs( self.defense_dfs, 1 )
-        
-        print( self.defens_dfs[ names[0] ].iloc[date_idx] )
-    
-    def select_offense(self, date_idx )
-        names = sort_dfs( self.offense_cfs, 2 )
-        
-        print("-----------")
-        print( self.offense_dfs[ names[0] ].iloc[date_idx] )
-        print("-----------")
-        print( self.offense_dfs[ names[1] ].iloc[date_idx] )
-        
-        
-    def sort_dfs(self, dfs, date_idx, count )
+
+    def select_defense(self, date_idx):
+        names = self.sort_dfs(self.defense_dfs, date_idx, 1)
+
+        print('[%s]   defense : [%s] -> [%lf]' % (
+            self.defense_dfs[names[0]].index[date_idx],
+            names[0],
+            self.defense_dfs[names[0]]['Profit'].iloc[date_idx+1] )  )
+
+
+        #print(self.defense_dfs[names[0]].iloc[date_idx])
+
+    def select_offense(self, date_idx):
+        names = self.sort_dfs(self.offense_dfs, date_idx, 2)
+
+        print('[%s]   offense : [%s %s] -> [%lf %lf]' % (
+            self.offense_dfs[names[0]].index[date_idx],
+            names[0], names[1],
+            self.offense_dfs[names[0]]['Profit'].iloc[date_idx+1] , self.offense_dfs[names[1]]['Profit'].iloc[date_idx+1] )  )
+
+    def sort_dfs(self, dfs, date_idx, count):
         oneday_df = pd.DataFrame()
-        
-        for key, val in dfs.items()
-            my_data = list(val.iloc[date_idx]) + [key] 
+
+        for key, val in dfs.items():
+            my_data = list(val.iloc[date_idx]) + [key]
             my_col = list(val.iloc[date_idx].index) + ['ticker_name']
-            
-            tmp = pd.DataFrame( data=[my_data], columns=my_col )
-            oneday_df = oneday_df.append( tmp )
-        
-        oneday_df = oneday_df.set_index('ticker_name', drop=False )
-        oneday_df = oneday_df.sort_values(by='score', ascending=False)
-        
+
+            tmp = pd.DataFrame(data=[my_data], columns=my_col)
+            oneday_df = oneday_df.append(tmp)
+
+        oneday_df = oneday_df.set_index('ticker_name', drop=False)
+        oneday_df = oneday_df.sort_values(by='Score', ascending=False)
+
         result = []
-        for idx in range(0,count)
-            result.append( oneday_df.iloc[idx]['ticker_name'] )
-         
+        for idx in range(0, count):
+            result.append(oneday_df.iloc[idx]['ticker_name'])
+
         return result
 
     def save_type_list(self):
-        for key, val in self.namesGrouop.items():
-            f = open( 'data/Z_%s_list.csv'%(key) , 'w', encoding='utf-8', newline='')
+        for key, val in self.groupsNames.items():
+            f = open('data/Z_%s_list.csv' % key, 'w', encoding='utf-8', newline='')
             wr = csv.writer(f)
             for name in val:
-                wr.writerow( [name] )
+                wr.writerow([name])
             f.close()
-
--- main.py
+++ main.py
@@ -1,94 +1,14 @@
-# import os
-import pandas_datareader as pdr
-import operator
 from TickerMng import TickerMng
 
 tickers = TickerMng()
-
-#mng.offense = ['SPY.US', 'IWM.US', 'QQQ.US', 'VGK.US', 'EWJ.US', 'VWO.US', 'VNQ.US', 'GSG.US', 'GLD.US', 'TLT.US', 'HYG.US', 'LQD.US']
-#mng.defense = ['SHV.US', 'IEF.US', 'UST.US']
-#mng.canary = ['BND.US', 'VWO.US']
-
 tickers.load_type_list()
-#tickers.download_stooq()
-#tickers.download_fdr()
+# tickers.download_stooq()
 tickers.load_dataframe()
 
-for date_idx in range(0,10):
+for date_idx in range( 12, len(tickers.offense_dfs['SPY.US'].index)-1  ) :
     if tickers.is_risky_canary(date_idx) :
         tickers.select_defense(date_idx)
     else :
         tickers.select_offense(date_idx)
         
 exit(0)
-# Download후에 저장하는방식
-# 값을 파일별로 분리
-# SPY등을 인자로 찾는다.
-
-def get_daa_score(ticker):
-    mon = pdr.get_data_stooq(ticker)
-    #mon.to_csv(ticker)
-
-    mon = mon['Close'].resample('M').first()
-    #mon.to_csv(ticker+"1")
-    #val = (mon[-1] - mon[-2])*12 + (mon[-1] - mon[-4])*4 + (mon[-1] - mon[-7])*2 + (mon[-1] - mon[-13])
-
-    #print(mon.pct_change(periods=1)[-1] )
-    #print("-------------")
-    #print(mon.pct_change(periods=6) )
-    val = mon.pct_change(periods=1)[-1] * 12 + mon.pct_change(periods=3)[-1] * 4 + mon.pct_change(periods=6)[-1] * 2 + mon.pct_change(periods=12)[-1]
-    print( "%s :  %lf " % (ticker , val) )
-    return val
-    #return val
-
-#aaa = pdr.get_data_stooq('SPY.US', chunksize='20')
-#print( aaa )
-#def get_daa_score(ticker):
-#    mon = pdr.StooqDailyReader(symbols=ticker,  )
-
-#offense = ['SPY', 'IWM', 'QQQ', 'VGK', 'EWJ', 'VWO', 'VNQ', 'GSG', 'GLD', 'TLT', 'HYG', 'LQD']
-#defense = ['SHV', 'IEF', 'UST']
-#canaria = ['BND', 'VWO']
-
-offense = ['SPY.US', 'IWM.US', 'QQQ.US', 'VGK.US', 'EWJ.US', 'VWO.US', 'VNQ.US', 'GSG.US', 'GLD.US', 'TLT.US', 'HYG.US', 'LQD.US']
-defense = ['SHV.US', 'IEF.US', 'UST.US']
-canaria = ['BND.US', 'VWO.US']
-
-#get_daa_score('SPY.US')
-#exit(0)
-
-offense_score = {}
-defense_score = {}
-canaria_score = {}
-
-for t in offense:
-    offense_score[t] = get_daa_score(t)
-offense_list = sorted(offense_score.items(), key=operator.itemgetter(1), reverse=True)
-
-for t in defense:
-    defense_score[t] = get_daa_score(t)
-
-defense_list = sorted(defense_score.items(), key=operator.itemgetter(1), reverse=True)
-
-for t in canaria:
-    canaria_score[t] = get_daa_score(t)
-
-level = 0
-
-for score in canaria_score.values():
-    if score > 0:
-        level += 1
-
-if level == 0:
-    print('수비 100%')
-    i = list(defense_list[0])
-    print('수비 : ', i[0], '  스코어 :', i[1])
-elif level == 1:
-    print('수비 50%, 공격 50%(6종목)')
-    print('수비 :', defense_list[0])
-    for i in offense_list[:6]:
-        print('공격 :', i[0], '  스코어 :', i[1])
-else:
-    print('공격 100%(6종목)')
-    for i in offense_list[:6]:
-        print('공격 : ', i[0], ' 스코어 :', i[1])
