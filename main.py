f42990c65e738398ddc6d043cccb2dc9213977e3
hanki8.choi <coffa80@naver.com>
2020-09-13 22:26:15 +0900

    DAA stable v1.1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from TickerMng import TickerMng
+from DAABackTest import DAABackTest
 
-tickers = TickerMng()
-tickers.load_type_list()
-# tickers.download_stooq()
-tickers.load_dataframe()
+daa = DAABackTest()
+daa.load_type_list()
+daa.download_stooq()
+daa.load_dataframe()
 
-tickers.show_daa_profit()
+daa.show_profit()
